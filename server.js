db122a2db01d2a3c76b05064d236f299ba51810d
Kai McConnell <mcconnka@whitman.edu>
2019-12-10 10:36:30 -0800

    AHAHHHHH WORK AGAIN PLEASE IM SORRY GIT I WONT PUSH TO MASTER EVER AGAIN.

--- server.js
+++ server.js
@@ -27,40 +27,40 @@ app.get('/', (request, response) => {
     // response.send(responseObj);
 })
 
-app.get('/location', (request, response) => {
-    // let city = request.query.data;
+// app.get('/location', (request, response) => {
+//     // let city = request.query.data;
 
-    // console.log(city);
-    // let locationObj = searchLatToLong(city);
-    // responseObj = {
-    //     "search_query": city,
-    //     "formatted_query": city,
-    //     "latitude": locationObj.latitude,
-    //     "longitude": locationObj.longitude
-    // }
-    // console.log(responseObj);
+//     // console.log(city);
+//     // let locationObj = searchLatToLong(city);
+//     // responseObj = {
+//     //     "search_query": city,
+//     //     "formatted_query": city,
+//     //     "latitude": locationObj.latitude,
+//     //     "longitude": locationObj.longitude
+//     // }
+//     // console.log(responseObj);
 
-    // response.send(responseObj);
-})
+//     // response.send(responseObj);
+// })
 
-searchLatToLong = function(searchQuery) {
-    // const geoData = require('./data/geo.json');
+// searchLatToLong = function(searchQuery) {
+//     // const geoData = require('./data/geo.json');
 
-    // for (let i = 0; i < geoData.results; i++) {
-    //     if (geoData.results[i] === searchQuery) {
-    //         return geoData.results[i].geometry.location
-    //     }
-    // }
-    // const geoDataResults = geoData.results[0];
+//     // for (let i = 0; i < geoData.results; i++) {
+//     //     if (geoData.results[i] === searchQuery) {
+//     //         return geoData.results[i].geometry.location
+//     //     }
+//     // }
+//     // const geoDataResults = geoData.results[0];
 
-    // const locationObj = new Location(city, geoDataResults);
-    // const locationObj = {
-    //   "search_query": city,
-    //   "formatted_query": geoDataResults.formatted_address,
-    //   "latitude": geoDataResults.geometry.location.lat,
-    //   "longitude": geoDataResults.geometry.location.lng
-    // }
-}
+//     // const locationObj = new Location(city, geoDataResults);
+//     // const locationObj = {
+//     //   "search_query": city,
+//     //   "formatted_query": geoDataResults.formatted_address,
+//     //   "latitude": geoDataResults.geometry.location.lat,
+//     //   "longitude": geoDataResults.geometry.location.lng
+//     // }
+// }
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
